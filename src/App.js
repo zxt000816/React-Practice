1715c805842b62d388a4aff60b07b1aa255598dc
zxt000816 <zyf13815628123@gmail.com>
2022-10-28 11:11:57 +0900

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import SearchIcon from './search.svg'
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=c8a01dff';
 
-// const movie1 = {
-//     "Title": "Italian Spiderman",
-//     "Year": "2007",
-//     "imdbID": "tt2705436",
-//     "Type": "movie",
-//     "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-// }
-
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
