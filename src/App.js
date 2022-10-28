d769441713fa3ef2c4721c9c7f6be2e30c929aee
zxt000816 <zyf13815628123@gmail.com>
2022-10-28 11:15:05 +0900

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import MovideCard from "./MovieCard";
 import './App.css'
+
 import SearchIcon from './search.svg'
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=c8a01dff';
